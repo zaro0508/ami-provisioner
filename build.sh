98372f553b280244520fc5d2721c7133d0ef8511
Khai Do <zaro0508@gmail.com>
2019-09-06 22:34:23 -0700

    attempt to fix build

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ do
   echo "${log}"
   # special case skip build if AWS contains an AMI with the same name
   echo "test1"
-  if [[ ${log} =~ "is used by an existing AMI" && ${status} -ne 0 ]]; then
+  if [[ ${log} =~ "name conflicts with an existing AMI" && ${status} -ne 0 ]]; then
     echo "test2"
     printf "\n\e[1;33m==> WARN: Skipped build, AMI already exists.\n\n"
   elif [[ ${status} -ne 0 ]]; then   # catch any other failure
